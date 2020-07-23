f67ce7989872855ee13b712c2dafd6610c4231fc
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2020-07-23 14:49:12 -0600

    use proper bech32 library

--- python/moneysocket/utl/bech32.py
+++ python/moneysocket/utl/bech32.py
@@ -3,32 +3,15 @@
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php
 
 import bitstring
+from bech32 import bech32_encode, bech32_decode, convertbits
 
-from moneysocket.utl.third_party.bech32 import bech32_encode
-from moneysocket.utl.third_party.bech32 import bech32_decode
 
 class Bech32():
     @staticmethod
-    def bitarray_to_u5(barr):
-        assert len(barr) % 5 == 0
-        ret = []
-        s = bitstring.ConstBitStream(barr)
-        while s.pos != s.len:
-            ret.append(s.read(5).uint)
-        return ret
-
-    @staticmethod
-    def u5_to_bitarray(arr):
-        ret = bitstring.BitArray()
-        for a in arr:
-            ret += bitstring.pack("uint:5", a)
-        return ret
-
-    @staticmethod
-    def encode_bytes(bytes_to_encode, hrp="msbeacon"):
-        while len(bytes_to_encode) % 5 != 0:
-            bytes_to_encode += b'\0'
-        return bech32_encode("ms", Bech32.bitarray_to_u5(bytes_to_encode))
+    def encode_bytes(bytes_to_encode, hrp):
+        converted = convertbits(bytes_to_encode, 8, 5, True)
+        assert converted
+        return bech32_encode(hrp, converted)
 
     @staticmethod
     def decode_bytes(bech32_string):
@@ -38,7 +21,7 @@ class Bech32():
             return None, None
         if not hrp:
             return None, None
-        decoded = Bech32.u5_to_bitarray(data).bytes
+        deconverted = convertbits(data, 5, 8, False)
+        assert deconverted
+        decoded = bytes(deconverted)
         return hrp, decoded
-
-
